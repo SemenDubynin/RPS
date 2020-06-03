8d2e42712d04a498c1c7e68fb8458e11b1a99da7
Semen <semendubynin@gmail.com>
2020-06-03 11:41:44 +0300

    Initial Commit

---
+++ RPS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RPS
+//
+//  Created by Дубынин Семён on 03.06.2020.
+//  Copyright © 2020 Дубынин Семён. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
