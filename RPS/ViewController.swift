1b57a2b67070d59e3cff8b69fe6ecb53f10cf9c1
Semen <semendubynin@gmail.com>
2020-06-08 14:59:48 +0300

    newCommitTwo

--- RPS/ViewController.swift
+++ RPS/ViewController.swift
@@ -9,12 +9,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var robotButton: UIButton!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var rockButton: UIButton!
+    @IBOutlet weak var paperButton: UIButton!
+    @IBOutlet weak var scissorsButton: UIButton!
+    @IBOutlet weak var playAgainButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        playAgainButton.isHidden = true
     }
-
-
+    func play(_ sign: Sign) {
+        let computerSign = randomSign()
+        robotButton.setTitle(computerSign.emoji, for: .normal)
+        
+        switch sign {
+        case .rock:
+            rockButton.isHidden = false
+            paperButton.isHidden = true
+            scissorsButton.isHidden = true
+        case .paper:
+            paperButton.isHidden = false
+            rockButton.isHidden =  true
+            scissorsButton.isHidden = true
+        case .scissors:
+            scissorsButton.isHidden = false
+            rockButton.isHidden = true
+            paperButton.isHidden = true
+        }
+        playAgainButton.isHidden = false
+        
+        let result = sign.getResult(computerSign)
+        switch result {
+        case .win:
+            statusLabel.text = "You Win!"
+            self.view.backgroundColor = UIColor.green
+        case .lose:
+            statusLabel.text = "You  lose!"
+            self.view.backgroundColor = UIColor.red
+        case .draw:
+            statusLabel.text = "Draw!"
+            self.view.backgroundColor = UIColor.yellow
+        case .start:
+            reset()
+        }
+    }
+    
+    func reset() {
+        statusLabel.text = "Rock - Paper - Scissors"
+        self.view.backgroundColor = UIColor.white
+        rockButton.isHidden = false
+        paperButton.isHidden = false
+        scissorsButton.isHidden = false
+        playAgainButton.isHidden = true
+    }
+    // MARK: - IBAction
+    
+    @IBAction func rockButtonPressed(_ sender: Any) {
+        play(.rock)
+    }
+    @IBAction func paperButtonPressed(_ sender: Any) {
+        play(.paper)
+    }
+    @IBAction func scissorsButtonPressed(_ sender: Any) {
+        play(.scissors)
+    }
+    @IBAction func playAgainButtonPressed(_ sender: Any) {
+        reset()
+    }
 }
 
